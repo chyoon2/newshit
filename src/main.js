0ca65e98b999400b1f6fae452967faf1906497a4
Chris Yoon <chy.yoon@gmail.com>
2020-06-29 22:18:44 -0700

    changes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Pet } from './pet.js';
-import { displayStat } from './display.js';
-
 
 $(document).ready(function(){
 
